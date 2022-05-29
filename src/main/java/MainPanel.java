a93ca0a034355bc4a07dbd2a2c2e427693ccf839
Hodaya Gamliel <Hodaya4799@gmail.com>
2022-05-29 16:49:00 +0300

    Filter not working yet

--- src/main/java/MainPanel.java
+++ src/main/java/MainPanel.java
@@ -3,11 +3,16 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class MainPanel extends JPanel {
     private JComboBox<String> classComboBox;
     private JComboBox<String> sexComboBox;
     private JComboBox<String> embarkedComboBox;
+    private String[] valuesOfText;
+    private String valueClass;
+    private String valueSex;
+    private String valueEmbarked;
     private static final int EMPTY = 0;
 
 
@@ -49,16 +54,19 @@ public class MainPanel extends JPanel {
                 Constants.COMBO_BOX_WIDTH, Constants.COMBO_BOX_HEIGHT);
         this.add(this.embarkedComboBox);
 
+        JTextField[] textFields = new JTextField[Constants.TEXT_FIELDS];
+
         int textField_y = 3 * Constants.MARGIN_Y;
         for (int i = 0; i < Constants.TEXT_FIELDS; i++) {
             JTextField jTextField = new JTextField();
             jTextField.setBounds(Constants.TEXT_FIELD_X, textField_y, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
             this.add(jTextField);
+            textFields[i] = jTextField;
             textField_y += Constants.MARGIN_Y;
         }
 
-        String[] textLabels = {"Passenger Class:", "Passenger Sex:", "Embarked:", "Passenger Number Range:", "Passenger Name:",
-                "Siblings/Spouses:", "Children/Parents:", "Ticket Number:", "Ticket Fare:", "Cabin:"};
+        String[] textLabels = {"Passenger Class:", "Passenger Sex:", "Embarked:", "Passenger Num Range, Min:", "Passenger Num Range, Max:",
+                "Passenger Name:", "Siblings/Spouses:", "Children/Parents:", "Ticket Number:", "Ticket Fare, Min:", "Ticket Fare, Max:", "Cabin:"};
 
         for (int j = 0; j < textLabels.length; j++) {
             JLabel jLabel = new JLabel(textLabels[j]);
@@ -67,18 +75,62 @@ public class MainPanel extends JPanel {
             y += Constants.MARGIN_Y;
         }
 
+        JButton filter = new JButton("Filter");
+        filter.setBounds(Constants.FILTER_X, Constants.FILTER_Y, Constants.FILTER_WIDTH, Constants.FILTER_HEIGHT);
+        this.add(filter);
+
+        //String valueNumRangeMin = "";
+        //String valueNumRangeMax = "";
+        //String valueName = "";
+        //String valueSibSP = "";
+        //String valueParch = "";
+        //String valueTicketNum = "";
+        //String valueTicketFareMin = "";
+        //String valueTicketFareMax = "";
+        //String valueCabin = "";
+        this.valuesOfText = new String[9];
+
+        filter.addActionListener((e) -> {
+            this.valueClass = classComboBox.getSelectedItem().toString();
+            this.valueSex = sexComboBox.getSelectedItem().toString();
+            this.valueEmbarked = embarkedComboBox.getSelectedItem().toString();
+            for (int i = 0; i < textFields.length; i++) {
+                this.valuesOfText[i] = textFields[i].getText();
+            }
+            List<Passenger> filteredList = filterResult(passengerData);
+            System.out.println(filteredList.size());
+
+        });
+
         this.classComboBox.addActionListener((e) -> {
-            //String value = classComboBox.getSelectedItem().toString();
-
-
-
+            this.valueClass = classComboBox.getSelectedItem().toString();
+        });
+        this.sexComboBox.addActionListener((e) -> {
+            this.valueSex = sexComboBox.getSelectedItem().toString();
+        });
+        this.embarkedComboBox.addActionListener((e) -> {
+            this.valueEmbarked = embarkedComboBox.getSelectedItem().toString();
         });
 
 
 
     }
 
-    
+
+    public List<Passenger> filterResult(ArrayList<Passenger> passangerData) {
+        return passangerData.stream().filter(passenger -> passenger.passengerInRange(this.valuesOfText[0], this.valuesOfText[1])).filter(passenger
+                -> passenger.containsName(this.valuesOfText[2])).filter(passenger -> passenger.sibSPMatch(this.valuesOfText[3])).filter(passenger ->
+                passenger.parchMatch(this.valuesOfText[4])).filter(passenger -> passenger.ticketNumMatch(this.valuesOfText[5])).filter(passenger ->
+                passenger.ticketFareMatch(this.valuesOfText[6], this.valuesOfText[7])).filter(passenger ->
+                passenger.cabinMatch(this.valuesOfText[8])).filter(passenger -> passenger.filterClass(this.valueClass)).filter(passenger ->
+                passenger.filterSex(this.valueSex)).filter(passenger -> passenger.filterEmbarked(this.valueEmbarked)).collect(Collectors.toList());
+
+        //return passangerData.stream().filter(passenger -> passenger.passengerInRange(this.valuesOfText[0],
+        //        this.valuesOfText[1])).filter(passenger -> passenger.containsName(this.valuesOfText[2])).filter(passenger
+        //        -> passenger.sibSPMatch(this.valuesOfText[3])).collect(Collectors.toList());
+    }
+
+
     private static Passenger convert(String data) {
         String[] dataToArr = data.split(",");
         int PassengerId = Integer.parseInt(dataToArr[0]);
--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -39,6 +39,163 @@ public class Passenger {
         return fullName;
     }
 
+
+    public boolean passengerInRange(String minRange, String maxRange) {
+        System.out.println("try");
+        try {
+            if (maxRange.equals(null) && minRange.equals(null)) {
+                return true;
+            }
+            int min = Constants.MIN;
+            int max = Constants.MAX;
+            if (!minRange.equals("")) {
+                min = Integer.parseInt(minRange);
+            }
+            if (!maxRange.equals("")) {
+                max = Integer.parseInt(maxRange);
+            }
+            return (this.passengerId <= max && this.passengerId >= min);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean containsName(String name) {
+        System.out.println("try");
+        try {
+            if (name.equals(null)) {
+                return true;
+            }
+            return (this.name.contains(name));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean sibSPMatch(String num) {
+        System.out.println("try");
+        try {
+            if (num.equals(null)) {
+                return true;
+            }
+            int num2 = Integer.parseInt(num);
+            return this.sibSp == num2;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean parchMatch(String num) {
+        System.out.println("try");
+        try {
+            if (num.equals(null)) {
+                return true;
+            }
+            int num2 = Integer.parseInt(num);
+            return this.parch == num2;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean ticketNumMatch(String num) {
+        System.out.println("try");
+        try {
+            if (num.equals(null)) {
+                return true;
+            }
+            int num2 = Integer.parseInt(num);
+            String temp = "";
+            for (int i = 0; i < this.ticket.length(); i++) {
+                if (Character.isDigit(this.ticket.charAt(i))) {
+                    temp += this.ticket.charAt(i);
+                }
+            }
+            int ticketNum = Integer.parseInt(temp);
+            return ticketNum == num2;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean ticketFareMatch(String min, String max) {
+        System.out.println("try");
+        try {
+            if (min.equals(null) && max.equals(null)) {
+                return true;
+            }
+            float minimum = Float.parseFloat(min);
+            float maximum = Float.parseFloat(max);
+            return this.fare >= minimum && this.fare <= maximum;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean cabinMatch(String cabin) {
+        System.out.println("try");
+        try {
+            if (cabin.equals(null)) {
+                return true;
+            }
+            String temp = "";
+            for (int i = 0; i < this.cabin.length(); i++) {
+                if (Character.isDigit(this.cabin.charAt(i))) {
+                    temp += this.cabin.charAt(i);
+                }
+            }
+            int cabinNum = Integer.parseInt(temp);
+            return cabinNum == Integer.parseInt(cabin);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean filterClass(String pClass) {
+        System.out.println("try");
+        if (pClass.equals(null)) {
+            return true;
+        }
+        switch (pClass) {
+            case Constants.ALL:
+                return true;
+            case Constants.FIRST:
+                if (this.pClass == Constants.FIRST_INT) {
+                    return true;
+                } break;
+            case Constants.SECOND:
+                if (this.pClass == Constants.SECOND_INT) {
+                    return true;
+                } break;
+            case Constants.THIRD:
+                if (this.pClass == Constants.THIRD_INT) {
+                    return true;
+                } break;
+        } return false;
+    }
+
+    public boolean filterSex(String sex) {
+        System.out.println("try");
+        try {
+            if (sex.equals(null)) {
+                return true;
+            }
+            return sex.equals(Constants.ALL) || sex.equals(this.sex);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean filterEmbarked(String embarked) {
+        System.out.println("try");
+        if (embarked.equals(null)) {
+            return true;
+        }
+        String thisEmbarked = this.embarked + "";
+        return embarked.equals(Constants.ALL) || embarked.equals(thisEmbarked);
+    }
+
+
     @Override
     public String toString() {
         return "Passenger{" +
